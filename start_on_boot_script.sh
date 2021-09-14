ea9ecc0b5eb359a4d6d1b228f600a02471499cb6
Edison Pizarro <edipizarro@uc.cl>
2021-09-13 21:57:02 -0300

    test

--- start_on_boot_script.sh
+++ start_on_boot_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo python3 track.py --source 0 --weights /yolov5/weights/yolov5s_7C.py --distance 40 --save-data-usb
+echo perceptron | sudo -S python3 track.py --source 0 --weights yolov5/weights/yolov5s_7C.pt --distance 40 --show-vid
